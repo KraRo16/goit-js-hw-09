aea95e709bf51edcc21f2002983c27b7e80a05e9
Krasnozhon Roman <105301264+KraRo16@users.noreply.github.com>
2022-10-09 15:47:07 +0200

    1 and 2 hw done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const btnColorStart = document.querySelector('button[data-start]');
+
+const btnColorStop = document.querySelector('button[data-stop]');
+
+btnColorStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    document.body.style.background = randomColor;
+  }, 1000);
+});
+
+btnColorStop.addEventListener('click', () => {
+  clearInterval(timerId);
+});
