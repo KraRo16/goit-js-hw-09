e34ecf2046bad0a1c13d80d8c75e071e89ca6b6d
KraRo16 <105301264+KraRo16@users.noreply.github.com>
2022-10-09 13:47:30 +0000

    Deploying to gh-pages from @ KraRo16/goit-js-hw-09@aea95e709bf51edcc21f2002983c27b7e80a05e9 🚀

---
+++ 01-color-switcher.3178b0cf.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){timerId=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.background=t}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerId)}))}();
+//# sourceMappingURL=01-color-switcher.3178b0cf.js.map
